1a4a22c8258711ca4b02edf46b14542221c0f04e
Francesca-Lisi <francescalisi2143@gmail.com>
2024-12-18 00:12:49 +0100

    feat: fix express and add path

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('ciaoo')
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+res.send('Server del mio blog')
+})
+
+app.listen(port, ()=>{
+console.log('in funzione')
+})
